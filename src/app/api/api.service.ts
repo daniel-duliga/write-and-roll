257c3f0c1e81e19a30441965c25aba9c4203c808
Daniel Duliga <daniel.vasile.duliga@gmail.com>
2021-10-12 19:19:55 +0300

    Add support for rolling dice from actions

--- src/app/api/api.service.ts
+++ src/app/api/api.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import { DiceUtil } from '../trpg/dice/dice.util';
+import { DiceWrapper } from '../trpg/dice/dice.wrapper';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
-
   constructor() { }
+
+  rollDice(formula: string): number {
+    return DiceUtil.rollDiceFormula(formula).sum;
+  }
 }
--- src/app/components/actions/action.service.ts
+++ src/app/components/actions/action.service.ts
@@ -1,12 +1,16 @@
 import { Injectable } from '@angular/core';
+import { ApiService } from 'src/app/api/api.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ActionService {
-  constructor() { }
+  constructor(
+    private api: ApiService
+  ) { }
 
   run(action: string): string {
+    const api = this.api;
     const result: string = eval(action);
     return result;
   }
