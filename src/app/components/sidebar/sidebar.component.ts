741234c24770cfb12fa7da63c369f7e26bac80ee
Daniel Duliga <daniel.vasile.duliga@gmail.com>
2021-10-16 23:04:19 +0300

    Sidebar component improvements

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -6,7 +6,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sidebar.component.css']
 })
 export class SidebarComponent implements OnInit {
-  expanded = false;
+  items: { link: string, tooltip: string, icon: string, text: string }[] = [
+    {
+      link: "/chronicles",
+      tooltip: "Chronicle",
+      icon: 'book',
+      text: 'Chronicles'
+    },
+    {
+      link: "/random-tables",
+      tooltip: "Random Tables",
+      icon: 'view_list',
+      text: 'Random Tables'
+    },
+    {
+      link: "/actions",
+      tooltip: "Actions",
+      icon: 'code',
+      text: 'Actions'
+    },
+    {
+      link: "/import-export",
+      tooltip: "Import/Export",
+      icon: 'save',
+      text: 'Import/Export'
+    },
+  ];
+  expanded = true;
 
   constructor() { }
 
