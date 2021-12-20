b4ee5587dfc5843cfccb6b161a7646b2a0a7b9e4
Daniel Duliga <daniel.vasile.duliga@gmail.com>
2021-12-20 21:33:31 +0200

    Process note content on editor open

--- src/app/components/editor/editor.component.ts
+++ src/app/components/editor/editor.component.ts
@@ -65,6 +65,7 @@ export class EditorComponent implements OnInit {
   ngAfterViewInit() {
     setTimeout(() => {
       this.configureCodeMirror();
+      this.postProcessCodeMirror();
       this.refresh();
     }, 250);
   }
