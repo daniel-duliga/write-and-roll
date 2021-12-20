38ccfe3c35695a78ca2e271f7049231f0d9cffc8
Daniel Duliga <daniel.vasile.duliga@gmail.com>
2021-12-20 22:09:22 +0200

    Support for auto-closing brackets

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
+//#region codemirror
 import 'codemirror/mode/javascript/javascript';
+
 import 'codemirror/mode/markdown/markdown';
 import 'codemirror/addon/edit/continuelist';
 
@@ -10,6 +12,9 @@ import 'codemirror/addon/fold/foldgutter';
 import 'codemirror/addon/fold/brace-fold';
 import 'codemirror/addon/fold/indent-fold';
 import './app/extensions/markdown-fold';
+
+import 'codemirror/addon/edit/closebrackets';
+//#endregion
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
